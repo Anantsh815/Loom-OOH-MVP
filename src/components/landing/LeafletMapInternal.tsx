02c5cf627b60321d859446d89c83b9e62eaa1894
Anantsh815 <anant@loom-ooh.local>
2026-01-13 17:56:15 +0530

    Customize map zoom control: bottom-right, neon cyan theme

--- src/components/landing/LeafletMapInternal.tsx
+++ src/components/landing/LeafletMapInternal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap, ZoomControl } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { useState, useEffect } from "react";
@@ -59,9 +59,11 @@ export default function LeafletMapInternal() {
                 center={[20.5937, 78.9629]}
                 zoom={5}
                 scrollWheelZoom={false}
+                zoomControl={false}
                 className="w-full h-full bg-[#050a14]"
                 attributionControl={false}
             >
+                <ZoomControl position="bottomright" />
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
                     url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
